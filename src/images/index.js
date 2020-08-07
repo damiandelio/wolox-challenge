1d1f779280294a518ee28960f19fd7fc45a5d364
damiandelio <damiandelio@gmail.com>
2020-08-06 21:14:34 -0300

    adds technologies container in home

---
+++ src/images/index.js
@@ -0,0 +1,3 @@
+export const IlustraHero = require('./Ic_ilustra_Hero.png')
+export const IlustraHero2x = require('./Ic_ilustra_Hero@2x.png')
+export const IlustraHero3x = require('./Ic_ilustra_Hero@3x.png')
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
-import IlustraHero from '../../images/Ic_ilustra_Hero.png'
+import { IlustraHero, IlustraHero2x, IlustraHero3x } from '../../images'
+import { ReactComponent as IcTecnologys } from '../../svgs/Ic_Tecnologys.svg'
 import styles from './HomePage.module.scss'
 
 export default function HomePage() {
@@ -17,8 +18,37 @@ export default function HomePage() {
                      <br />
                      <strong className={styles.woloxText}>Wolox</strong>
                   </h2>
-                  <img src={IlustraHero} alt='Wolox tech illustration' />
+                  <picture>
+                     <source
+                        media='(min-width: 50rem)'
+                        srcSet={`${IlustraHero} 1x, ${IlustraHero2x} 2x, ${IlustraHero3x} 3x`}
+                     />
+                     <img src={IlustraHero} alt='Wolox tech illustration' />
+                  </picture>
+               </div>
+               <div className={styles.technologiesContainer}>
+                  <p>
+                     Estamos buscando
+                     <br />
+                     para incorporar gente
+                     <br />
+                     increíble para estas
+                     <br />
+                     tecnologías:
+                  </p>
+                  <IcTecnologys />
+               </div>
+               <div className={styles.woloxersBanner}>
                   <div></div>
+                  <div>
+                     <p>
+                        Trabajamos para
+                        <br />
+                        convertir ideas en
+                        <br />
+                        productos.
+                     </p>
+                  </div>
                </div>
             </div>
          </main>
